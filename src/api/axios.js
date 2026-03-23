50cf5c893fed0fa71989601a44e1b6a6bdb2b6af
hamadwiem <hamadwiem@gmail.com>
2026-03-23 21:28:13 +0100

    Fix CORS config for Render deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_API_URL || '',
     headers: {
         'Content-Type': 'application/json'
     }
@@ -16,4 +16,3 @@ api.interceptors.request.use(config => {
 })
 
 export default api
-
